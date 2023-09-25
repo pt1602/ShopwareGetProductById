fa4623a81745aa35cb6b93810b7833666cc70421
Philipp Tuchardt <philipp.tuchardt@nahketing.de>
2023-09-25 12:05:20 +0200

    feat(core): updated for shopware 6.5

--- src/Subscriber/GetProductById.php
+++ src/Subscriber/GetProductById.php
@@ -5,18 +5,18 @@ declare(strict_types=1);
 namespace ShopwareGetProductById\Subscriber;
 
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\System\SalesChannel\Entity\SalesChannelRepositoryInterface;
+use Shopware\Core\System\SalesChannel\Entity\SalesChannelRepository;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class GetProductById extends AbstractExtension
 {
-    private SalesChannelRepositoryInterface $salesChannelProductRepository;
+    private SalesChannelRepository $salesChannelProductRepository;
 
     /**
      *   Construct
      */
-    public function __construct(SalesChannelRepositoryInterface $salesChannelProductRepository)
+    public function __construct(SalesChannelRepository $salesChannelProductRepository)
     {
         $this->salesChannelProductRepository = $salesChannelProductRepository;
     }
